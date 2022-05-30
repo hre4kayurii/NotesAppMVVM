3ebef81414348bf583685fd096bfd3b44074dfb3
ukropBandera <ukrop.bandera@gmail.com>
2022-05-30 19:34:51 +0300

    test 3

--- app/src/main/java/com/kawa/notesappmvvm/MainActivity.kt
+++ app/src/main/java/com/kawa/notesappmvvm/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kawa.notesappmvvm.navigation.NotesNavHost
 import com.kawa.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
@@ -18,15 +18,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppMVVMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
 
-                    NotesNavHost()
+                Scaffold(
+                    topBar = {
 
-                }
+                        TopAppBar(
+                            title = {
+                                Text(text = "Notes App")
+                            },
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White,
+                            elevation = 12.dp
+
+                        )
+                    },
+                    content = {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.background
+                        ) {
+                            NotesNavHost()
+                        }
+                    }
+
+                )
+
             }
         }
     }
